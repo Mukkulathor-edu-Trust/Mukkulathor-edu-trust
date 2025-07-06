3ac0faf7dd2c7b5f4f50005fc2e38097aa61bb41
Mukkulathor-edu-Trust <mukkulathor156@gmail.com>
2025-07-06 17:37:15 +0530

    base link update

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Mukkulathor-edu-trust">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
